4d25f12fbb06e7afa1fc8bd733db351d55df395f
Alberto-T <alberto.torres.cancino@gmail.com>
2024-04-12 19:16:07 -0400

    Null undefined was added

---
+++ tipos/null-undefined.js
@@ -0,0 +1,6 @@
+"use strict";
+(() => {
+    // StrictNullChecks => false en tsconfig.json
+    let isActive = null;
+    // console.log(nada);
+})();
---
+++ tipos/null-undefined.ts
@@ -0,0 +1,9 @@
+(() =>{
+
+    // StrictNullChecks => false en tsconfig.json
+    let isActive:boolean = null;
+
+    // console.log(nada);
+
+
+})()
