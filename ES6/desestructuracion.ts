ad02b9287d52ac573ceb016fe6cc28b856cd8857
Alberto-T <alberto.torres.cancino@gmail.com>
2024-04-16 17:08:59 -0400

    Desestructuration of objects

--- ES6/desestructuracion.ts
+++ ES6/desestructuracion.ts
@@ -1,11 +1,38 @@
 (()=> {
 
-    const avengers = {
+    type Avengers = {
+        nick: string;
+        ironman: string;
+        vision: string;
+        activo: boolean;
+        poder: number;
+    }
+
+    const avengers: Avengers = {
         nick: 'Samuel L. Jackson',
         ironman: 'Robert Downey Jr.',
         vision: 'Paul Bettany',
         activo: true,
         poder: 1500
     }
+
+    // const { poder, vision } = avengers;
+    // console.log( poder.toFixed(2), vision.toUpperCase() );
+    
+    //* Ejemplo uno
+    // const printAvenger = ( avengers: Avengers ) => {
+    //     console.log( avengers.vision );
+    // }
+
+    // printAvenger( avengers )
+
+    //* Ejemplo dos
+
+    const printAvenger = ( { ironman, ...resto }: Avengers ) => {
+        console.log( ironman, resto );
+    }
+
+    printAvenger( avengers )
+
 
 })()
