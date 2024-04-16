fb6ca4a6086a572e3ad69ca0f3d4bb097c966771
Alberto-T <alberto.torres.cancino@gmail.com>
2024-04-16 19:31:50 -0400

    Inheritance, super and extends

--- classes/basica.ts
+++ classes/basica.ts
@@ -29,13 +29,13 @@
     }
 
 // las propiedades staticas se pueden llamar de manera global llamando a la clase, pero no a la instancia y utilizando la propiedad
-    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
-    console.log( antman );
-    // console.log( Avenger.avgAge );
-    console.log(Avenger.avgAge);
+    // const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
+    // console.log( antman );
+    // // console.log( Avenger.avgAge );
+    // console.log(Avenger.avgAge);
 
-    console.log( antman.bio() );
+    // console.log( antman.bio() );
 
-    console.log(Avenger.getAvgAge());
+    // console.log(Avenger.getAvgAge());
 
 })()
