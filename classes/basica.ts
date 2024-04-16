fa677d0146bf311042407167ff406d6edae26d66
Alberto-T <alberto.torres.cancino@gmail.com>
2024-04-16 19:10:40 -0400

    Short form of asign properties

--- classes/basica.ts
+++ classes/basica.ts
@@ -2,23 +2,29 @@
 
     class Avenger {
 
-        private name: string;
-        public team: string;
-        public realName?: string;
+        // private name: string;
+        // private team: string;
+        // public realName?: string;
         static avgAge: number = 35;
 
-        constructor( name: string, team: string, realName?: string ) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
+        constructor( private name: string,
+            private team: string,
+            public realName?: string,
+            avgAge: number = 35, 
+        ) {
+            // this.name = name;
+            // this.team = team;
+            // this.realName = realName;
+            Avenger.avgAge = avgAge;
         }
 
     }
 
 // las propiedades staticas se pueden llamar de manera global llamando a la clase, pero no a la instancia y utilizando la propiedad
-    const antman: Avenger = new Avenger('Antman', 'Capitan');
+    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log( antman );
     // console.log( Avenger.avgAge );
+    console.log(Avenger.avgAge);
 
 
 
