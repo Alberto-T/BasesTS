f6381269df21954a030801dde12678cb490664fe
Alberto-T <alberto.torres.cancino@gmail.com>
2024-04-16 14:45:33 -0400

    Multiples types allowed

---
+++ objetos/union-types.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    let myCustomVariable = 'Alberto';
+    console.log(myCustomVariable);
+    console.log(typeof myCustomVariable);
+    myCustomVariable = 20;
+    console.log(typeof myCustomVariable);
+    myCustomVariable = {
+        name: 'Bruce',
+        age: 43,
+        powers: [1]
+    };
+    console.log(myCustomVariable);
+    console.log(typeof myCustomVariable);
+})();
